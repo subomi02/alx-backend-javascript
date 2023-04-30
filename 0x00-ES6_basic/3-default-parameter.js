31c9088563951c405cd3d26a5ee017627e63c0ad
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-04-30 21:34:29 +0100

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,1 @@
-export default( initialNumber + expansion1989 = 89 + expansion2019 = 19)
+export default (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
