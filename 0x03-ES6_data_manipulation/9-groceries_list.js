78df9e87a1d4e58415b25b9aebd3f547b3fdb22b
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 20:34:54 +0100

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,15 +1,10 @@
-/**
- * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {Map<string, Number>}
- */
 export default function groceriesList() {
-  const values = [
+  const results = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-  return new Map(values);
+  return new Map(results);
 }
