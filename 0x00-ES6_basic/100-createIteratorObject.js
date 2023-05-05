3f34f363fec8980b36549a19d034df3f9b0fadf7
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 19:13:39 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,18 +1,17 @@
-//Olasubomi
 export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees).flat();
+  const emyees = Object.values(report.allEmployees).flat();
   let index = 0;
   
   return {
     next() {
-      if (index >= employees.length) {
+      if (index >= emyees.length) {
         return { done: true };
       }
       
-      const value = employees[index];
-      index++;
+      const value = emyees[index];
+      index += 1;
       
       return { value, done: false };
-    }
+    },
   };
 }
