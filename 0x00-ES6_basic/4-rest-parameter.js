0595f505c7bc09c116662949a038692d73d71cf7
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 18:14:56 +0100

    Update 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,3 @@
-// Define a function called returnHowManyArguments using the rest parameter syntax.
-//SubomiAdetunji
 export default function returnHowManyArguments(...args) {
   return args.length;
 }
