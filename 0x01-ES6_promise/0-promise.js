5af85c5a47ff661292c0aba53488d04237b03de4
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 19:57:11 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,3 @@
-
-Here's an example of how to create a Promise using the prototype function getResponseFromAPI():
-
-javascript
-Copy code
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform some asynchronous operation
-    const data = { message: "Hello, world!" };
-    resolve(data);
-
-    // If the operation fails
-    const error = "Failed to connect to API.";
-    reject(error);
-  });
+  return Promise.resolve(true);
 }
